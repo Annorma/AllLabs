7c4bd8542b3ab7839ef69806f2421478880e622f
Annorma <80634257+Annorma@users.noreply.github.com>
2023-11-10 11:14:53 +0100

    Added comments to Lab_4.

--- Lab_04_Zad_01-02-New/Program.cs
+++ Lab_04_Zad_01-02-New/Program.cs
@@ -12,19 +12,19 @@ namespace Lab_04_Zad_01_02_New
 {
     //----------============ Zad 02 ============----------↓
 
-    public interface IDisplayable
+    public interface IDisplayable // Interfejs dla obiektów, które mogą być wyświetlane jako ciągi znaków
     {
         string ToString();
     }
 
-    public static class DisplayActionExtensions
+    public static class DisplayActionExtensions // Metody rozszerzające do wyświetlania obiektów
     {
-        public static void Print<T>(this T obj)
+        public static void Print<T>(this T obj) // Wyświetlanie obiektu przy użyciu Console.WriteLine
         {
             Console.WriteLine(obj != null ? obj.ToString() : "null");
         }
 
-        public static void Print<T>(this IList<T> list)
+        public static void Print<T>(this IList<T> list) // Wyświetlanie listy obiektów przy użyciu metody rozszerzającej Print
         {
             foreach (var item in list)
             {
@@ -33,43 +33,48 @@ namespace Lab_04_Zad_01_02_New
         }
     }
 
-    public interface IContainer { }
+    public interface IContainer { } // Interfejs dla obiektów kontenerowych
 
-    public static class CrudActionExtensions
+    public static class CrudActionExtensions // Metody rozszerzające do operacji CRUD (Create, Read, Update, Delete) na obiektach kontenerowych
     {
-        public static IList<T> Set<T>(this IContainer containerObject)
+        public static IList<T> Set<T>(this IContainer containerObject) // Pobiera listę z obiektu kontenerowego na podstawie właściwości
         {
-            var collectionProperty = containerObject.GetType().GetProperties().FirstOrDefault(p => p.PropertyType == typeof(IList<T>));
+            var collectionProperty = containerObject.GetType().GetProperties().FirstOrDefault(p => p.PropertyType == typeof(IList<T>)); // Sprawdzamy, czy obiekt posiada właściwość będącą listą obiektów typu T
             if (collectionProperty != null)
             {
+                // Jeśli tak, zwracamy tę listę
                 return (IList<T>)collectionProperty.GetValue(containerObject);
             }
 
+            // Jeśli nie znaleziono właściwości, zwracamy null
             return null;
         }
 
-        public static void ForEach<T>(this IList<T> list, Action<T> action)
+        public static void ForEach<T>(this IList<T> list, Action<T> action)  // Wykonuje akcję dla każdego elementu na liście
         {
-            var collectionProperty = list.GetType().GetProperties().FirstOrDefault(p => p.PropertyType == typeof(IList<T>));
+            var collectionProperty = list.GetType().GetProperties().FirstOrDefault(p => p.PropertyType == typeof(IList<T>)); // Sprawdzamy, czy lista posiada właściwość będącą listą obiektów typu T
             if (collectionProperty != null)
             {
-                var collection = (IList<T>)collectionProperty.GetValue(list);
-                foreach (var item in collection)
+                var collection = (IList<T>)collectionProperty.GetValue(list); // Jeśli tak, uzyskujemy dostęp do listy
+
+                foreach (var item in collection) // Wykonujemy podaną akcję dla każdego elementu na liście
                 {
                     action(item);
                 }
             }
         }
 
-        public static T Get<T>(this IContainer container, Func<T, bool>? searchPredicate = null)
+        public static T Get<T>(this IContainer container, Func<T, bool>? searchPredicate = null) // Pobranie elementu z obiektu kontenerowego na podstawie predykatu wyszukiwania
         {
-            var collectionProperty = container.GetType().GetProperties().FirstOrDefault(p => p.PropertyType == typeof(IList<T>));
+            var collectionProperty = container.GetType().GetProperties().FirstOrDefault(p => p.PropertyType == typeof(IList<T>)); // Sprawdzamy, czy obiekt posiada właściwość będącą listą obiektów typu T
             if (collectionProperty != null)
             {
-                var collection = (IList<T>)collectionProperty.GetValue(container);
+                var collection = (IList<T>)collectionProperty.GetValue(container); // Jeśli tak, uzyskujemy dostęp do listy
+
+                // Jeśli podano predykat wyszukiwania, zwracamy pierwszy pasujący element, w przeciwnym razie pierwszy element listy
                 if (searchPredicate != null)
                 {
-                    return collection.FirstOrDefault(searchPredicate);
+                    return collection.FirstOrDefault(searchPredicate); 
                 }
                 else
                 {
@@ -77,15 +82,17 @@ namespace Lab_04_Zad_01_02_New
                 }
             }
 
-            return default(T);
+            return default(T); // Jeśli nie znaleziono właściwości lub listy, zwracamy domyślną wartość dla typu T
         }
 
-        public static IList<T> GetList<T>(this IContainer container, Func<T, bool>? searchPredicate = null)
+        public static IList<T> GetList<T>(this IContainer container, Func<T, bool>? searchPredicate = null) // Pobranie listy elementów z obiektu kontenerowego na podstawie predykatu wyszukiwania
         {
-            var collectionProperty = container.GetType().GetProperties().FirstOrDefault(p => p.PropertyType == typeof(IList<T>));
+            var collectionProperty = container.GetType().GetProperties().FirstOrDefault(p => p.PropertyType == typeof(IList<T>)); // Sprawdzamy, czy obiekt posiada właściwość będącą listą obiektów typu T
             if (collectionProperty != null)
             {
-                var collection = (IList<T>)collectionProperty.GetValue(container);
+                var collection = (IList<T>)collectionProperty.GetValue(container); // Jeśli tak, uzyskujemy dostęp do listy
+
+                // Jeśli podano predykat wyszukiwania, zwracamy listę pasujących elementów, w przeciwnym razie całą listę
                 if (searchPredicate != null)
                 {
                     return collection.Where(searchPredicate).ToList();
@@ -96,44 +103,51 @@ namespace Lab_04_Zad_01_02_New
                 }
             }
 
+            // Jeśli nie znaleziono właściwości lub listy, zwracamy nową pustą listę dla typu T
             return new List<T>();
         }
 
-        public static T Add<T>(this IContainer container, T obj)
+        public static T Add<T>(this IContainer container, T obj) // Dodanie elementu do obiektu kontenerowego
         {
-            var collectionProperty = obj.GetType().GetProperties().FirstOrDefault(p => p.PropertyType == typeof(IList<T>));
+            var collectionProperty = obj.GetType().GetProperties().FirstOrDefault(p => p.PropertyType == typeof(IList<T>)); // Sprawdzamy, czy obiekt posiada właściwość będącą listą obiektów typu T
             if (collectionProperty != null)
             {
+                // Jeśli tak, uzyskujemy dostęp do listy i dodajemy do niej nowy element
                 var collection = (IList<T>)collectionProperty.GetValue(obj);
                 collection.Add(obj);
             }
 
-            return obj;
+            return obj; // Zwracamy dodany obiekt
         }
 
-        public static bool Remove<T>(this IContainer container, Func<T, bool> searchFn)
+        public static bool Remove<T>(this IContainer container, Func<T, bool> searchFn) // Usunięcie elementów z obiektu kontenerowego na podstawie funkcji wyszukiwania
         {
-            var collectionProperty = container.GetType().GetProperties().FirstOrDefault(p => p.PropertyType == typeof(IList<T>));
+            var collectionProperty = container.GetType().GetProperties().FirstOrDefault(p => p.PropertyType == typeof(IList<T>)); // Sprawdzamy, czy obiekt posiada właściwość będącą listą obiektów typu T
             if (collectionProperty != null)
             {
-                var collection = (IList<T>)collectionProperty.GetValue(container);
-                var itemsToRemove = collection.Where(searchFn).ToList();
+                var collection = (IList<T>)collectionProperty.GetValue(container); // Jeśli tak, uzyskujemy dostęp do listy
+                var itemsToRemove = collection.Where(searchFn).ToList(); // Znajdujemy elementy do usunięcia na podstawie funkcji wyszukiwania
+
+                // Usuwamy znalezione elementy z listy
                 foreach (var item in itemsToRemove)
                 {
                     collection.Remove(item);
                 }
 
+                // Zwracamy informację, czy cokolwiek zostało usunięte
                 return itemsToRemove.Count > 0;
             }
 
+            // Jeśli nie znaleziono właściwości lub listy, zwracamy false
             return false;
         }
 
-        public static IContainer AddRange<T>(this IContainer container, IList<T> listOfElements)
+        public static IContainer AddRange<T>(this IContainer container, IList<T> listOfElements) // Dodanie zakresu elementów do obiektu kontenerowego
         {
-            var collectionProperty = container.GetType().GetProperties().FirstOrDefault(p => p.PropertyType == typeof(IList<T>));
+            var collectionProperty = container.GetType().GetProperties().FirstOrDefault(p => p.PropertyType == typeof(IList<T>)); // Sprawdzamy, czy obiekt posiada właściwość będącą listą obiektów typu T
             if (collectionProperty != null)
             {
+                // Jeśli tak, uzyskujemy dostęp do listy i dodajemy do niej cały zakres elementów
                 var collection = (IList<T>)collectionProperty.GetValue(container);
                 foreach (var item in listOfElements)
                 {
@@ -141,6 +155,7 @@ namespace Lab_04_Zad_01_02_New
                 }
             }
 
+            // Zwracamy obiekt kontenerowy po dodaniu zakresu elementów
             return container;
         }
     }
@@ -151,12 +166,14 @@ namespace Lab_04_Zad_01_02_New
     {
         //----------============ Zad 01 ============----------↓
 
-        public abstract class Person : IDisplayable, IContainer
+        public abstract class Person : IDisplayable, IContainer // Abstrakcyjna klasa reprezentująca osobę, implementująca interfejsy IDisplayable i IContainer
         {
+            // Właściwości reprezentujące dane osobowe
             public string FirstName { get; set; }
             public string LastName { get; set; }
             public DateTime DateOfBirth { get; set; }
 
+            // Konstruktor inicjalizujący dane osobowe
             public Person(string firstName, string lastName, DateTime dateOfBirth)
             {
                 FirstName = firstName;
@@ -164,56 +181,67 @@ namespace Lab_04_Zad_01_02_New
                 DateOfBirth = dateOfBirth;
             }
 
+            // Przesłonięta metoda ToString zwracająca sformatowany ciąg znaków reprezentujący osobę
             public override string ToString()
             {
                 return $"{FirstName} {LastName} - {DateOfBirth.ToShortDateString()}";
             }
         }
 
-        public class Lecturer : Person
+        public class Lecturer : Person // Klasa reprezentująca wykładowcę, dziedzicząca po klasie Person
         {
+            // Dodatkowe właściwości charakterystyczne dla wykładowcy
             public string AcademicTitle { get; set; }
             public string Position { get; set; }
+
+            // Konstruktor inicjalizujący dane wykładowcy i dziedziczący dane osobowe z klasy Person
             public Lecturer(string firstName, string lastName, DateTime dateOfBirth, string academicTitle, string position) : base(firstName, lastName, dateOfBirth)
             {
                 AcademicTitle = academicTitle;
                 Position = position;
             }
 
+            // Przesłonięta metoda ToString zwracająca sformatowany ciąg znaków reprezentujący wykładowcę
             public override string ToString()
             {
                 return $"Lecturer | {base.ToString()} {Position}\n{AcademicTitle} ";
             }
         }
 
-        public class Student : Person
+        public class Student : Person // Klasa reprezentująca studenta, dziedzicząca po klasie Person
         {
-            private static int id;
+            private static int id; // Prywatna statyczna zmienna identyfikująca studenta
+
+            // Dodatkowe właściwości charakterystyczne dla studenta
             public IList<FinalGrade> Grades { get; set; } = new List<FinalGrade>();
             public int Semestr { get; set; }
             public int Group { get; set; }
             public int IndexId { get; set; }
             public string Specialization { get; set; }
             public double AverageGrades { get; }
+            // Konstruktor inicjalizujący dane studenta i dziedziczący dane osobowe z klasy Person
             public Student(string firstName, string lastName, DateTime dateOfBirth, string specialization, int group, int semestr = 1) : base(firstName, lastName, dateOfBirth)
             {
                 Semestr = semestr;
                 Group = group;
                 Specialization = specialization;
             }
+            // Przesłonięta metoda ToString zwracająca sformatowany ciąg znaków reprezentujący studenta
             public override string ToString()
             {
                 return $"Student | {base.ToString()} {Semestr} {Group} {Specialization} - {IndexId}\n{AverageGrades}";
             }
         }
 
-        public class Subject
+        public class Subject // Klasa reprezentująca przedmiot
         {
+            // Właściwości reprezentujące dane przedmiotu
             public string Name { get; set; }
             public string Specialization { get; set; }
             public int Semestr { get; set; }
             public int HoursCount { get; set; }
 
+            // Konstruktor inicjalizujący dane przedmiotu
             public Subject(string name, string specialization, int semestr, int hoursCount)
             {
                 Name = name;
@@ -221,40 +249,52 @@ namespace Lab_04_Zad_01_02_New
                 Semestr = semestr;
                 HoursCount = hoursCount;
             }
+
+            // Przesłonięta metoda ToString zwracająca sformatowany ciąg znaków reprezentujący przedmiot
             public override string ToString()
             {
                 return $"Subject | {Name} {Specialization} {Semestr} - {HoursCount}";
             }
         }
 
-        public class FinalGrade
+        public class FinalGrade // Klasa reprezentująca ocenę końcową
         {
+            // Właściwości reprezentujące dane oceny końcowej
             public Subject Subject { get; set; }
             public DateTime Date { get; set; }
             public double Value { get; set; }
+            // Konstruktor inicjalizujący dane oceny końcowej
             public FinalGrade(Subject subject, double value, DateTime date)
             {
                 Subject = subject;
                 Date = date;
                 Value = value;
             }
+
+            // Przesłonięta metoda ToString zwracająca sformatowany ciąg znaków reprezentujący ocenę końcową
             public override string ToString()
             {
                 return $"FinalGrade:\n{Subject} | {Date.ToShortDateString()} {Value}";
             }
         }
 
+        // Klasa reprezentująca jednostkę organizacyjną
         public class OrganizationUnit : IDisplayable, IContainer
         {
+            // Właściwości reprezentujące dane jednostki organizacyjnej
             public string Name { get; set; }
             public string Address { get; set; }
             public IList<Lecturer> Lecturers { get; set; } = new List<Lecturer>();
+
+            // Konstruktor inicjalizujący dane jednostki organizacyjnej
             public OrganizationUnit(string name, string address, IList<Lecturer> lecturers)
             {
                 Name = name;
                 Address = address;
                 Lecturers = lecturers;
             }
+
+            // Przesłonięta metoda ToString zwracająca sformatowany ciąg znaków reprezentujący jednostkę organizacyjną
             public override string ToString()
             {
                 string lecturers = "\nLecturers:\n";
@@ -269,13 +309,16 @@ namespace Lab_04_Zad_01_02_New
         }
 
 
-        public class Department : IDisplayable, IContainer
+        public class Department : IDisplayable, IContainer // Klasa reprezentująca wydział lub dział w instytucji edukacyjnej
         {
+            // Właściwości reprezentujące dane wydziału lub działu
             public string Name { get; set; }
             public Person Dean { get; set; }
             public IList<OrganizationUnit> OrganizationUnits { get; set; } = new List<OrganizationUnit>();
             public IList<Subject> Subjects { get; set; } = new List<Subject>();
             public IList<Student> Students { get; set; } = new List<Student>();
+
+            // Konstruktor inicjalizujący dane wydziału lub działu
             public Department(string name, Person dean, IList<Subject> subjects, IList<Student> students)
             {
                 Name = name;
@@ -284,6 +327,7 @@ namespace Lab_04_Zad_01_02_New
                 Students = students;
             }
 
+            // Przesłonięta metoda ToString zwracająca sformatowany ciąg znaków reprezentujący wydział lub dział
             public override string ToString()
             {
                 return $"Department | {Name} {Dean}";
@@ -365,9 +409,7 @@ namespace Lab_04_Zad_01_02_New
 
             //----------============ Zad 02 ============----------↑
 
-
             Console.ReadKey();
-
         }
     }
 }
