5acdda5be9316cf8e3eedb395a27e0c0a7ad239d
Annorma <80634257+Annorma@users.noreply.github.com>
2023-11-18 15:19:59 +0100

    Added Lab_5.

---
+++ Lab5.BLL/Animal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab5.BLL
+{
+    public class Animal
+    {
+        protected string _foodType;
+        protected int _legsCount;
+        protected string _origin;
+        protected string _species;
+
+        public Animal(string foodType, int legsCount, string origin, string species)
+        {
+            FoodType = foodType;
+            LegsCount = legsCount;
+            Origin = origin;
+            Species = species;
+        }
+
+        public string FoodType { get => _foodType; set => _foodType = value; }
+        public int LegsCount { get => _legsCount; set => _legsCount = value; }
+        public string Origin { get => _origin; set => _origin = value; }
+        public string Species { get => _species; set => _species = value; }
+    }
+}
---
+++ Lab5.BLL/Bird.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lab5.BLL
+{
+    public class Bird : Animal
+    {
+        public double Wingspan { get; set; }
+        public double Endurance { get; set; }
+        public Bird(string foodType, int legsCount, string origin, string species, double wingspan, double endurance) : base(foodType, legsCount, origin, species)
+        {
+            Wingspan = wingspan;
+            Endurance = endurance;
+        }
+
+        public void Fly()
+        {
+            Console.WriteLine($"Bird can fly max: {Wingspan * Endurance} km");
+        }
+    }
+}
